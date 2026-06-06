2ec9152629015be5941a1feb39c6e9478f3f56d1
Mabiala D Antonny <antonnymabiala@gmail.com>
2026-06-06 17:16:53 +0300

    I built the brands section

--- apex-motors/src/pages/home/components/brandsSection/brandsSection.tsx
+++ apex-motors/src/pages/home/components/brandsSection/brandsSection.tsx
@@ -2,8 +2,35 @@ import { component$ } from "@builder.io/qwik";
 import './brandsSection.scss';
 
 const BrandsSection = component$(() => {
+
+    const carBrands = [
+        "Porsche",
+        "Mercedes-Benz",
+        "BMW",
+        "Audi",
+        "Lexus",
+        "Tesla",
+        "Land Rover",
+        "Jaguar",
+        "Maserati",
+        "Bentley"
+    ];
     return (
-        <section class="brands">Brands Section</section>
+        <section class="brands">
+            <div class="container">
+                <div class="section-header">
+                    <h2>Premium <span class="gold-text">Brands</span></h2>
+                    <p>Authorized dealer for the world's finest automotive manufacturers</p>
+                </div>
+                <div class="brands__scroll">
+                    {
+                        carBrands.map((brand) => (
+                            <div class="brand-item">{brand}</div>
+                        ))
+                    }
+                </div>
+            </div>
+        </section>
     )
 });
 
